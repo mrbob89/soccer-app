913b7f79a57304266dbc2dcac19bfb92a4fa8e2b
Roman Boboshko <romanboboshko89@gmail.com>
2017-01-18 14:05:28 +0200

    Add ui components

---
+++ client/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import App from '../imports/ui/App.jsx';
+
+injectTapEventPlugin();
+
+Meteor.startup(() => {
+    render(
+        <App />,
+        document.getElementById('root')
+    );
+});
