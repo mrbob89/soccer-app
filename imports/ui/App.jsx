112c2888f7fbec8ac1d1a5f2ced986eb0648fdb5
Roman Boboshko <romanboboshko89@gmail.com>
2017-01-18 14:24:54 +0200

    Add more markup in react components

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+
+import Player from './Player.jsx';
+import TeamList from './TemaList.jsx';
+import TeamStats from './TeamStats.jsx';
 
 export default class App extends Component {
     render() {
         return (
             <MuiThemeProvider>
-                <RaisedButton label="Button"/>
+                <div className="container">
+                    <AppBar
+                        title="Soccer Application"
+                        iconClassNameRight="muidocs-icon-navigation-expand-more"
+                        showMenuIconButton={false}
+                    />
+                    <div className="row">
+                        <div className="col s12 m7"><Player /></div>
+                        <div className="col s12 m5"><TeamList /></div>
+                        <div className="col s12 m5"><TeamStats /></div>
+                    </div>
+                </div>
             </MuiThemeProvider>
         );
     }
