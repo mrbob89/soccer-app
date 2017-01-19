a8ea244a223c1925953b8b576e530d7052e65691
Roman Boboshko <romanboboshko89@gmail.com>
2017-01-19 19:51:43 +0200

    Add delete functionality

--- imports/ui/TeamList.jsx
+++ imports/ui/TeamList.jsx
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import Avatar from 'material-ui/Avatar';
 import { ListItem } from 'material-ui/List';
 import ActionDeleteForever from 'material-ui/svg-icons/action/delete-forever';
+import { red500 } from 'material-ui/styles/colors';
 
 export default class TeamList extends Component {
     updateCurrentPlayer(player) {
         this.props.updateCurrentPlayer(player);
+    }
+
+    deletePlayer(playerId) {
+        Meteor.call('deletePlayer', playerId, (error) => {
+            if(error) {
+                alert("Oups, something went wrong: " + error.reason);
+            } else {
+                console.log('Deleted...');
+            }
+        });
     }
 
     render() {
@@ -15,6 +26,8 @@ export default class TeamList extends Component {
             <ListItem
                 primaryText={player.name}
                 leftAvatar={<Avatar src="player.jpg"/>}
+                rightIcon={<ActionDeleteForever hoverColor={red500}
+                    onClick={this.deletePlayer.bind(this, player._id)}/>}
                 onClick={this.updateCurrentPlayer.bind(this, this.props.player)}
             />
         );
